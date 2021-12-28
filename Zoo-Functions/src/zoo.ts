6c61ca274dac756b033fc6d401d5ab9da1294867
Arthur-Jr <arthurjr1906@gmail.com>
2021-12-28 17:18:36 -0300

    Adiciona o primeiro desafio.

---
+++ Zoo-Functions/src/zoo.ts
@@ -0,0 +1,75 @@
+import data from './data';
+const { species } = data;
+
+function getSpeciesByIds(...ids: string[]) {
+  if(ids.length > 0) {
+    return ids.map((id) => {
+      return (species.find((specie) => specie.id === id));
+    });
+  }
+  return [];
+}
+
+function getAnimalsOlderThan(animal, age) {
+  // seu código aqui
+}
+
+function getEmployeeByName(employeeName) {
+  // seu código aqui
+}
+
+function createEmployee(personalInfo, associatedWith) {
+  // seu código aqui
+}
+
+function isManager(id) {
+  // seu código aqui
+}
+
+function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+  // seu código aqui
+}
+
+function countAnimals(species) {
+  // seu código aqui
+}
+
+function calculateEntry(entrants) {
+  // seu código aqui
+}
+
+function getAnimalMap(options) {
+  // seu código aqui
+}
+
+function getSchedule(dayName) {
+  // seu código aqui
+}
+
+function getOldestFromFirstSpecies(id) {
+  // seu código aqui
+}
+
+function increasePrices(percentage) {
+  // seu código aqui
+}
+
+function getEmployeeCoverage(idOrName) {
+  // seu código aqui
+}
+
+export {
+  calculateEntry,
+  getSchedule,
+  countAnimals,
+  getAnimalMap,
+  getSpeciesByIds,
+  getEmployeeByName,
+  getEmployeeCoverage,
+  addEmployee,
+  isManager,
+  getAnimalsOlderThan,
+  getOldestFromFirstSpecies,
+  increasePrices,
+  createEmployee,
+};
